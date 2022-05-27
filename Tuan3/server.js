c119be2696de1c373d89eecb6c6c3be917ba19e7
Cao Trung <caodinhtrung2k.it@gmail.com>
2022-05-27 23:51:40 +0700

    Tuần 3

---
+++ Tuan3/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var server = express();
+
+server.use(express.static(__dirname + '/public'));
+
+server.set('views', './views/pages');
+server.set('view engine', 'ejs');
+
+// index page 
+server.get('/', function(req, res) {
+    res.render('index', {})
+});
+// time page
+server.get('/time', function(req, res) {
+    res.render('time', {})
+});
+// day page
+server.get('/day', function(req, res) {
+    res.render('day', {})
+});
+
+server.listen(8080, function() {
+  console.log("Listening on 8080");
+});
